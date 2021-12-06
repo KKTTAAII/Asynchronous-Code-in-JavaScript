b5dbc7d93f197d77d4677179ca02d49124d2d57c
KKTTAAII <krataias65@gmail.com>
2021-12-05 22:57:36 -0700

    further study w/o UI

---
+++ Further_Study/app.js
@@ -0,0 +1,49 @@
+const url = "https://pokeapi.co/api/v2/pokemon/";
+let allPokemonCount;
+let randomPokemonsURLs = [];
+let selectSpeciesURLs = [];
+
+function getPokemons() {
+  axios.get("https://pokeapi.co/api/v2/pokemon/")
+    .then(res => {
+      allPokemonCount = res.data.count;
+      return axios.get(
+        `https://pokeapi.co/api/v2/pokemon/?limit=${allPokemonCount}`
+      );
+    })
+    .then(res => {
+      let allPokemons = res.data.results;
+      for (let i = 0; i < 3; i++) {
+        let randomPokemon =
+          allPokemons[Math.floor(Math.random() * allPokemons.length)];
+        randomPokemonsURLs.push(axios.get(randomPokemon.url));
+      }
+    })
+    .then(() => {
+      Promise.all(randomPokemonsURLs)
+        .then(res =>
+          res.forEach(p => {
+            selectSpeciesURLs.push(axios.get(p.data.species.url));
+          })
+        )
+        .then(() => {
+          Promise.all(selectSpeciesURLs).then(res => {
+            res.forEach(specie => {
+              let name;
+              let text;
+              for (let i = 0; i < specie.data.flavor_text_entries.length; i++) {
+                if(specie.data.flavor_text_entries[i].language.name === "en"){
+                    name = specie.data.name;
+                    text = specie.data.flavor_text_entries[i].flavor_text
+                }
+              }
+              console.log(`${name}: ${text}`);
+            });
+          });
+        })
+        .catch(err => console.log("Error", err))
+    })
+    .catch(err => console.log(err));
+}
+
+getPokemons();
